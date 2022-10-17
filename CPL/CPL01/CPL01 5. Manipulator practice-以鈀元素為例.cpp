cd285fb489ffe7f93c7192b4dfa5696fafe69af7
ChenBingWei1201 <49435609+ChenBingWei1201@users.noreply.github.com>
2022-10-17 15:03:07 +0800

    Add files via upload

---
+++ CPL/CPL01/CPL01 5. Manipulator practice-以鈀元素為例.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <iomanip> // You're going to use the functions in this header file, so you must have this line.
+
+using namespace std;
+
+int main(){
+    int num = 46;
+    double mass = 106.42;
+    double melt = 1828.05;
+    double boil = 3236.00;
+    double expansion = 0.0000118;
+    cout << "Atomic Number (hex) = "<< hex << num << endl;
+    cout << "Atomic Number (hex, withbase) = " << showbase << num << endl;
+    cout << "Atomic Number (hex, withbase, uppercase) = " << uppercase << num << endl;
+    cout.unsetf(ios::hex|ios::uppercase);
+    cout << "Standard Atomic Weight = "  << fixed << setprecision(6) << mass << endl;
+    cout << "Melting Point (K) = "  << fixed << setprecision(2) << melt << endl;
+    cout << "Boiling Point (K) = "  << fixed << setprecision(2) << boil << endl;
+    cout << "Thermal Expansion = "  << scientific << expansion << endl;  
+    return 0;
+}
